fd4940c0a5f90d48faebc10dbda71d3f56c4b9b3
jennmald <jmaldonad@bnl.gov>
2025-06-26 15:09:23 -0400

    fix dtype

--- src/cditools/merlin.py
+++ src/cditools/merlin.py
@@ -29,6 +29,15 @@ class MerlinTiffPlugin(TIFFPlugin, FileStoreBulkReadable, FileStoreTIFF,
             return 1
         else:
             return self.parent.cam.num_images.get()
+        
+    def describe(self):
+        ret = super().describe()
+        key = self.parent._image_name
+        cam_dtype = self.parent.cam.data_type.get(as_string=True)
+        type_map = {'UInt8': '|u1', 'UInt16': '<u2', 'Float32':'<f4', "Float64":'<f8'}
+        if cam_dtype in type_map:
+            ret[key].setdefault('dtype_str', type_map[cam_dtype])
+        return ret
 
 
 class MerlinDetectorCam(CamBase):
