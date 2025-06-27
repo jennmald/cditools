3332a9a227a41cb24131726f912937ec2e98c8c3
jennmald <jmaldonad@bnl.gov>
2025-06-27 08:27:34 -0400

    debugging

--- src/cditools/merlin.py
+++ src/cditools/merlin.py
@@ -34,6 +34,7 @@ class MerlinTiffPlugin(TIFFPlugin, FileStoreBulkReadable, FileStoreTIFF,
         ret = super().describe()
         key = self.parent._image_name
         cam_dtype = self.parent.cam.data_type.get(as_string=True)
+        print(cam_dtype)
         type_map = {'UInt8': '|u1', 'UInt16': '<u2', 'Float32':'<f4', "Float64":'<f8'}
         if cam_dtype in type_map:
             ret[key].setdefault('dtype_str', type_map[cam_dtype])
@@ -84,6 +85,7 @@ class MerlinFileStoreHDF5(FileStorePluginBase, FileStoreBulkReadable):
         ret = super().describe()
         key = self.parent._image_name
         cam_dtype = self.parent.cam.data_type.get(as_string=True)
+        print(cam_dtype)
         type_map = {'UInt8': '|u1', 'UInt16': '<u2', 'Float32':'<f4', "Float64":'<f8'}
         if cam_dtype in type_map:
             ret[key].setdefault('dtype_str', type_map[cam_dtype])
@@ -111,6 +113,7 @@ class HDF5PluginWithFileStore(HDF5Plugin, MerlinFileStoreHDF5):
         ret = super().describe()
         key = self.parent._image_name
         cam_dtype = self.parent.cam.data_type.get(as_string=True)
+        print(cam_dtype)
         type_map = {'UInt8': '|u1', 'UInt16': '<u2', 'Float32':'<f4', "Float64":'<f8'}
         if cam_dtype in type_map:
             ret[key].setdefault('dtype_str', type_map[cam_dtype])
