fbf452299cd65d1e597900dfcddb2de40a48093e
Thomas Hopkins <thopkins1@bnl.gov>
2025-07-30 14:49:56 -0400

    Updated motors to reflect desired ophyd objects and new PV strings (#11)
    * Updated motors to reflect desired ophyd objects and new PV strings
    
    * Remove ophyd generators
    
    * Remove leading '2' in attribute name
    
    * Use empty string for PV suffix so it passes down properly
    
    * Remove print (tests pass locally but GHA is too slow...)
    
    * Force higher version of ophyd for testing due to new features used
    
    * ruff fixes

--- src/cditools/motors.py
+++ src/cditools/motors.py
@@ -2,264 +2,319 @@ from __future__ import annotations
 
 from ophyd import Component as Cpt  # type: ignore[import-not-found]
 from ophyd import Device, EpicsMotor
-
-# Auto-generated Ophyd device classes
-
-
-class SltWB1(Device):
-    """Ophyd Device for Slt WB1"""
-
-    i = Cpt(EpicsMotor, "-Ax:I}Mtr")
-    o = Cpt(EpicsMotor, "-Ax:O}Mtr")
-    b = Cpt(EpicsMotor, "-Ax:B}Mtr")
-    t = Cpt(EpicsMotor, "-Ax:T}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class FltrDM1(Device):
-    """Ophyd Device for Fltr DM1"""
-
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-
-
-class FSVPM(Device):
-    """Ophyd Device for FS VPM"""
-
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-
-
-class FSHPM(Device):
-    """Ophyd Device for FS HPM"""
-
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-
-
-class FSDM2(Device):
-    """Ophyd Device for FS DM2"""
-
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-
-
-class MonoDMM(Device):
-    """Ophyd Device for Mono DMM"""
-
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    bragg = Cpt(EpicsMotor, "-Ax:Bragg}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    tz = Cpt(EpicsMotor, "-Ax:TZ}Mtr")
-    fp = Cpt(EpicsMotor, "-Ax:FP}Mtr")
-    fr = Cpt(EpicsMotor, "-Ax:FR}Mtr")
-
-
-class MonoHDCM(Device):
-    """Ophyd Device for Mono HDCM"""
-
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    bragg = Cpt(EpicsMotor, "-Ax:Bragg}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    fp = Cpt(EpicsMotor, "-Ax:FP}Mtr")
-
-
-class SltVPM(Device):
-    """Ophyd Device for Slt VPM"""
-
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class SltHPM(Device):
-    """Ophyd Device for Slt HPM"""
-
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class MirVPM(Device):
-    """Ophyd Device for Mir VPM"""
-
-    yuc = Cpt(EpicsMotor, "-Ax:YUC}Mtr")
-    ydi = Cpt(EpicsMotor, "-Ax:YDI}Mtr")
-    ydo = Cpt(EpicsMotor, "-Ax:YDO}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ub = Cpt(EpicsMotor, "-Ax:UB}Mtr")
-    db = Cpt(EpicsMotor, "-Ax:DB}Mtr")
-    bnd = Cpt(EpicsMotor, "-Ax:Bnd}Mtr")
-    bndoff = Cpt(EpicsMotor, "-Ax:BndOff}Mtr")
-
-
-class IMDM2(Device):
-    """Ophyd Device for IM DM2"""
-
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-
-
-class MirHPM(Device):
-    """Ophyd Device for Mir HPM"""
-
-    yuc = Cpt(EpicsMotor, "-Ax:YUC}Mtr")
-    ydi = Cpt(EpicsMotor, "-Ax:YDI}Mtr")
-    ydo = Cpt(EpicsMotor, "-Ax:YDO}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    ub = Cpt(EpicsMotor, "-Ax:UB}Mtr")
-    db = Cpt(EpicsMotor, "-Ax:DB}Mtr")
-    bnd = Cpt(EpicsMotor, "-Ax:Bnd}Mtr")
-    bndoff = Cpt(EpicsMotor, "-Ax:BndOff}Mtr")
-    xu = Cpt(EpicsMotor, "-Ax:XU}Mtr")
-    xd = Cpt(EpicsMotor, "-Ax:XD}Mtr")
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    yaw = Cpt(EpicsMotor, "-Ax:Yaw}Mtr")
-
-
-class SltDM3(Device):
-    """Ophyd Device for Slt DM3"""
-
-    i = Cpt(EpicsMotor, "-Ax:I}Mtr")
-    o = Cpt(EpicsMotor, "-Ax:O}Mtr")
-    b = Cpt(EpicsMotor, "-Ax:B}Mtr")
-    t = Cpt(EpicsMotor, "-Ax:T}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class BPMDM3(Device):
-    """Ophyd Device for BPM DM3"""
-
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    foil = Cpt(EpicsMotor, "-Ax:Foil}Mtr")
-
-
-class FSDM3(Device):
-    """Ophyd Device for FS DM3"""
-
-    fs = Cpt(EpicsMotor, "-Ax:FS}Mtr")
-
-
-class MirKBv(Device):
-    """Ophyd Device for Mir KBv"""
-
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-    yuc = Cpt(EpicsMotor, "-Ax:YUC}Mtr")
-    ydi = Cpt(EpicsMotor, "-Ax:YDI}Mtr")
-    ydo = Cpt(EpicsMotor, "-Ax:YDO}Mtr")
-    yaw = Cpt(EpicsMotor, "-Ax:Yaw}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    tz = Cpt(EpicsMotor, "-Ax:TZ}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    fs = Cpt(EpicsMotor, "-Ax:FS}Mtr")
-
-
-class MirKBh(Device):
-    """Ophyd Device for Mir KBh"""
-
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-    yuc = Cpt(EpicsMotor, "-Ax:YUC}Mtr")
-    ydi = Cpt(EpicsMotor, "-Ax:YDI}Mtr")
-    ydo = Cpt(EpicsMotor, "-Ax:YDO}Mtr")
-    yaw = Cpt(EpicsMotor, "-Ax:Yaw}Mtr")
-    roll = Cpt(EpicsMotor, "-Ax:Roll}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    tz = Cpt(EpicsMotor, "-Ax:TZ}Mtr")
-    pitch = Cpt(EpicsMotor, "-Ax:Pitch}Mtr")
-    fs = Cpt(EpicsMotor, "-Ax:FS}Mtr")
-
-
-class BPMDM4(Device):
-    """Ophyd Device for BPM DM4"""
-
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-
-
-class WndExit(Device):
-    """Ophyd Device for Wnd Exit"""
-
-    tx = Cpt(EpicsMotor, "-Ax:TX}Mtr")
-    ty = Cpt(EpicsMotor, "-Ax:TY}Mtr")
-
-
-class Gon1(Device):
-    """Ophyd Device for Gon 1"""
-
-    rx1 = Cpt(EpicsMotor, "-Ax:Rx1}Mtr")
-    rz1 = Cpt(EpicsMotor, "-Ax:Rz1}Mtr")
-    rx2 = Cpt(EpicsMotor, "-Ax:Rx2}Mtr")
-    rz2 = Cpt(EpicsMotor, "-Ax:Rz2}Mtr")
-    y = Cpt(EpicsMotor, "-Ax:Y}Mtr")
-    ry = Cpt(EpicsMotor, "-Ax:Ry}Mtr")
-    x1 = Cpt(EpicsMotor, "-Ax:X1}Mtr")
-    z1 = Cpt(EpicsMotor, "-Ax:Z1}Mtr")
-    x2 = Cpt(EpicsMotor, "-Ax:X2}Mtr")
-    z2 = Cpt(EpicsMotor, "-Ax:Z2}Mtr")
-    rx3 = Cpt(EpicsMotor, "-Ax:Rx3}Mtr")
-    rz3 = Cpt(EpicsMotor, "-Ax:Rz3}Mtr")
-    x3 = Cpt(EpicsMotor, "-Ax:X3}Mtr")
-    y3 = Cpt(EpicsMotor, "-Ax:Y3}Mtr")
-    z3 = Cpt(EpicsMotor, "-Ax:Z3}Mtr")
-    visual = Cpt(EpicsMotor, "-Ax:Visual}Mtr")
-    xp = Cpt(EpicsMotor, "-Ax:XP}Mtr")
-    yp = Cpt(EpicsMotor, "-Ax:YP}Mtr")
-    zp = Cpt(EpicsMotor, "-Ax:ZP}Mtr")
-
-
-class SltBCUU(Device):
-    """Ophyd Device for Slt BCUU"""
-
-    i = Cpt(EpicsMotor, "-Ax:I}Mtr")
-    o = Cpt(EpicsMotor, "-Ax:O}Mtr")
-    b = Cpt(EpicsMotor, "-Ax:B}Mtr")
-    t = Cpt(EpicsMotor, "-Ax:T}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class SltBCUD(Device):
-    """Ophyd Device for Slt BCUD"""
-
-    i = Cpt(EpicsMotor, "-Ax:I}Mtr")
-    o = Cpt(EpicsMotor, "-Ax:O}Mtr")
-    b = Cpt(EpicsMotor, "-Ax:B}Mtr")
-    t = Cpt(EpicsMotor, "-Ax:T}Mtr")
-    hg = Cpt(EpicsMotor, "-Ax:HG}Mtr")
-    hc = Cpt(EpicsMotor, "-Ax:HC}Mtr")
-    vg = Cpt(EpicsMotor, "-Ax:VG}Mtr")
-    vc = Cpt(EpicsMotor, "-Ax:VC}Mtr")
-
-
-class Qstar1(Device):
-    """Ophyd Device for Qstar 1"""
-
-    ax1 = Cpt(EpicsMotor, "-Ax:1}Mtr")
-    ax2 = Cpt(EpicsMotor, "-Ax:2}Mtr")
-    ax3 = Cpt(EpicsMotor, "-Ax:3}Mtr")
+from ophyd import DynamicDeviceComponent as DDC
+
+
+class DM1(Device):
+    """Ophyd Device for DM1"""
+
+    slit = DDC(
+        {
+            "ib": (EpicsMotor, "Slt:WB1-Ax:I}Mtr", {}),
+            "ob": (EpicsMotor, "Slt:WB1-Ax:O}Mtr", {}),
+            "bb": (EpicsMotor, "Slt:WB1-Ax:B}Mtr", {}),
+            "tb": (EpicsMotor, "Slt:WB1-Ax:T}Mtr", {}),
+            "hg": (EpicsMotor, "Slt:WB1-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:WB1-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:WB1-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:WB1-Ax:VC}Mtr", {}),
+        }
+    )
+    filt = Cpt(EpicsMotor, "Fltr:DM1-Ax:Y}Mtr")
+
+
+class VPM(Device):
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "FS:VPM-Ax:Y}Mtr", {}),
+        }
+    )
+    slit = DDC(
+        {
+            "hg": (EpicsMotor, "Slt:VPM-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:VPM-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:VPM-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:VPM-Ax:VC}Mtr", {}),
+        }
+    )
+    mir = DDC(
+        {
+            "us_j": (EpicsMotor, "Mir:VPM-Ax:YUC}Mtr", {}),
+            "ib_j": (EpicsMotor, "Mir:VPM-Ax:YDI}Mtr", {}),
+            "ob_j": (EpicsMotor, "Mir:VPM-Ax:YDO}Mtr", {}),
+            "p": (EpicsMotor, "Mir:VPM-Ax:Pitch}Mtr", {}),
+            "r": (EpicsMotor, "Mir:VPM-Ax:Roll}Mtr", {}),
+            "y": (EpicsMotor, "Mir:VPM-Ax:TY}Mtr", {}),
+            "x": (EpicsMotor, "Mir:VPM-Ax:TX}Mtr", {}),
+            "us_b": (EpicsMotor, "Mir:VPM-Ax:UB}Mtr", {}),
+            "ds_b": (EpicsMotor, "Mir:VPM-Ax:DB}Mtr", {}),
+            "bend": (EpicsMotor, "Mir:VPM-Ax:Bnd}Mtr", {}),
+            "bend_off": (EpicsMotor, "Mir:VPM-Ax:BndOff}Mtr", {}),
+        }
+    )
+
+
+class HPM(Device):
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "FS:HPM-Ax:Y}Mtr", {}),
+        }
+    )
+    slit = DDC(
+        {
+            "hg": (EpicsMotor, "Slt:HPM-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:HPM-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:HPM-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:HPM-Ax:VC}Mtr", {}),
+        }
+    )
+    mir = DDC(
+        {
+            "us_j": (EpicsMotor, "Mir:HPM-Ax:YUC}Mtr", {}),
+            "ib_j": (EpicsMotor, "Mir:HPM-Ax:YDI}Mtr", {}),
+            "ob_j": (EpicsMotor, "Mir:HPM-Ax:YDO}Mtr", {}),
+            "p": (EpicsMotor, "Mir:HPM-Ax:Pitch}Mtr", {}),
+            "r": (EpicsMotor, "Mir:HPM-Ax:Roll}Mtr", {}),
+            "y": (EpicsMotor, "Mir:HPM-Ax:TY}Mtr", {}),
+            "us_b": (EpicsMotor, "Mir:HPM-Ax:UB}Mtr", {}),
+            "ds_b": (EpicsMotor, "Mir:HPM-Ax:DB}Mtr", {}),
+            "bend": (EpicsMotor, "Mir:HPM-Ax:Bnd}Mtr", {}),
+            "bend_off": (EpicsMotor, "Mir:HPM-Ax:BndOff}Mtr", {}),
+            "us_x": (EpicsMotor, "Mir:HPM-Ax:XU}Mtr", {}),
+            "ds_x": (EpicsMotor, "Mir:HPM-Ax:XD}Mtr", {}),
+            "x": (EpicsMotor, "Mir:HPM-Ax:TX}Mtr", {}),
+            "yaw": (EpicsMotor, "Mir:HPM-Ax:Yaw}Mtr", {}),
+        }
+    )
+
+
+class DM2(Device):
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "FS:DM2-Ax:Y}Mtr", {}),
+        }
+    )
+    foil = Cpt(EpicsMotor, "IM:DM2-Ax:Y}Mtr")
+
+
+class DMM(Device):
+    h = Cpt(EpicsMotor, "Mono:DMM-Ax:TX}Mtr")
+    v = Cpt(EpicsMotor, "Mono:DMM-Ax:TY}Mtr")
+    bragg = Cpt(EpicsMotor, "Mono:DMM-Ax:Bragg}Mtr")
+    mlm1 = DDC(
+        {
+            "r": (EpicsMotor, "Mono:DMM-Ax:Roll}Mtr", {}),
+            "fr": (EpicsMotor, "Mono:DMM-Ax:FR}Mtr", {}),
+        }
+    )
+    mgap = Cpt(EpicsMotor, "Mono:DMM-Ax:HG}Mtr")
+    mlm2 = DDC(
+        {
+            "p": (EpicsMotor, "Mono:DMM-Ax:Pitch}Mtr", {}),
+            "fp": (EpicsMotor, "Mono:DMM-Ax:FP}Mtr", {}),
+        }
+    )
+    zoff = Cpt(EpicsMotor, "Mono:DMM-Ax:TZ}Mtr")
+
+
+class DCM(Device):
+    h = Cpt(EpicsMotor, "Mono:HDCM-Ax:TX}Mtr")
+    v = Cpt(EpicsMotor, "Mono:HDCM-Ax:TY}Mtr")
+    bragg = Cpt(EpicsMotor, "Mono:HDCM-Ax:Bragg}Mtr")
+    cgap = Cpt(EpicsMotor, "Mono:HDCM-Ax:HG}Mtr")
+    c2 = DDC(
+        {
+            "p": (EpicsMotor, "Mono:HDCM-Ax:Pitch}Mtr", {}),
+            "r": (EpicsMotor, "Mono:HDCM-Ax:Roll}Mtr", {}),
+            "fp": (EpicsMotor, "Mono:HDCM-Ax:FP}Mtr", {}),
+        }
+    )
+
+
+class DM3(Device):
+    slit = DDC(
+        {
+            "ib": (EpicsMotor, "Slt:DM3-Ax:I}Mtr", {}),
+            "ob": (EpicsMotor, "Slt:DM3-Ax:O}Mtr", {}),
+            "bb": (EpicsMotor, "Slt:DM3-Ax:B}Mtr", {}),
+            "tb": (EpicsMotor, "Slt:DM3-Ax:T}Mtr", {}),
+            "hg": (EpicsMotor, "Slt:DM3-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:DM3-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:DM3-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:DM3-Ax:VC}Mtr", {}),
+        }
+    )
+    bpm = DDC(
+        {
+            "x": (EpicsMotor, "BPM:DM3-Ax:TX}Mtr", {}),
+            "y": (EpicsMotor, "BPM:DM3-Ax:TY}Mtr", {}),
+            "foil": (EpicsMotor, "BPM:DM3-Ax:Foil}Mtr", {}),
+        }
+    )
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "FS:DM3-Ax:FS}Mtr", {}),
+        }
+    )
+
+
+class VKB(Device):
+    slit = DDC(
+        {
+            "hg": (EpicsMotor, "Slt:KBv-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:KBv-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:KBv-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:KBv-Ax:VC}Mtr", {}),
+        }
+    )
+    mir = DDC(
+        {
+            "us_j": (EpicsMotor, "Mir:KBv-Ax:YUC}Mtr", {}),
+            "ib_j": (EpicsMotor, "Mir:KBv-Ax:YDI}Mtr", {}),
+            "ob_j": (EpicsMotor, "Mir:KBv-Ax:YDO}Mtr", {}),
+            "yaw": (EpicsMotor, "Mir:KBv-Ax:Yaw}Mtr", {}),
+            "r": (EpicsMotor, "Mir:KBv-Ax:Roll}Mtr", {}),
+            "y": (EpicsMotor, "Mir:KBv-Ax:TY}Mtr", {}),
+            "x": (EpicsMotor, "Mir:KBv-Ax:TX}Mtr", {}),
+            "z": (EpicsMotor, "Mir:KBv-Ax:TZ}Mtr", {}),
+            "p": (EpicsMotor, "Mir:KBv-Ax:Pitch}Mtr", {}),
+        }
+    )
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "FS:KBv-Ax:FS}Mtr", {}),
+        }
+    )
+
+
+class HKB(Device):
+    slit = DDC(
+        {
+            "hg": (EpicsMotor, "Slt:KBh-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:KBh-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:KBh-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:KBh-Ax:VC}Mtr", {}),
+        }
+    )
+    mir = DDC(
+        {
+            "us_j": (EpicsMotor, "Mir:KBh-Ax:YUC}Mtr", {}),
+            "ib_j": (EpicsMotor, "Mir:KBh-Ax:YDI}Mtr", {}),
+            "ob_j": (EpicsMotor, "Mir:KBh-Ax:YDO}Mtr", {}),
+            "yaw": (EpicsMotor, "Mir:KBh-Ax:Yaw}Mtr", {}),
+            "r": (EpicsMotor, "Mir:KBh-Ax:Roll}Mtr", {}),
+            "y": (EpicsMotor, "Mir:KBh-Ax:TY}Mtr", {}),
+            "x": (EpicsMotor, "Mir:KBh-Ax:TX}Mtr", {}),
+            "z": (EpicsMotor, "Mir:KBh-Ax:TZ}Mtr", {}),
+            "p": (EpicsMotor, "Mir:KBh-Ax:Pitch}Mtr", {}),
+        }
+    )
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "Mir:KBh-Ax:FS}Mtr", {}),
+        }
+    )
+
+
+class KB(Device):
+    vkb = Cpt(VKB, "")
+    hkb = Cpt(HKB, "")
+    win = DDC(
+        {
+            "x": (EpicsMotor, "Wnd:Exit-Ax:TX}Mtr", {}),
+            "y": (EpicsMotor, "Wnd:Exit-Ax:TY}Mtr", {}),
+        }
+    )
+
+
+class DM4(Device):
+    bpm = DDC(
+        {
+            "x": (EpicsMotor, "BPM:DM4-Ax:TX}Mtr", {}),
+            "y": (EpicsMotor, "BPM:DM4-Ax:TY}Mtr", {}),
+        }
+    )
+
+
+class SAM(Device):
+    c_sm = DDC(
+        {
+            "lrx": (EpicsMotor, "Gon:1-Ax:Rx1}Mtr", {}),
+            "lrz": (EpicsMotor, "Gon:1-Ax:Rz1}Mtr", {}),
+        }
+    )
+    c_lg = DDC(
+        {
+            "lrx": (EpicsMotor, "Gon:1-Ax:Rx2}Mtr", {}),
+            "lrz": (EpicsMotor, "Gon:1-Ax:Rz2}Mtr", {}),
+        }
+    )
+    ly = Cpt(EpicsMotor, "Gon:1-Ax:Y}Mtr")
+    ry = Cpt(EpicsMotor, "Gon:1-Ax:Ry}Mtr")
+    t_sm = DDC(
+        {
+            "lx": (EpicsMotor, "Gon:1-Ax:X1}Mtr", {}),
+            "lz": (EpicsMotor, "Gon:1-Ax:Z1}Mtr", {}),
+        }
+    )
+    t_lg = DDC(
+        {
+            "lx": (EpicsMotor, "Gon:1-Ax:X2}Mtr", {}),
+            "lz": (EpicsMotor, "Gon:1-Ax:Z2}Mtr", {}),
+        }
+    )
+    lfx = Cpt(EpicsMotor, "Gon:1-Ax:XP}Mtr")
+    lfy = Cpt(EpicsMotor, "Gon:1-Ax:YP}Mtr")
+    lfz = Cpt(EpicsMotor, "Gon:1-Ax:ZP}Mtr")
+
+
+class GON(Device):
+    sam = Cpt(SAM, "")
+    align = DDC(
+        {
+            "rx": (EpicsMotor, "Gon:1-Ax:Rx3}Mtr", {}),
+            "rz": (EpicsMotor, "Gon:1-Ax:Rz3}Mtr", {}),
+            "x": (EpicsMotor, "Gon:1-Ax:X3}Mtr", {}),
+            "y": (EpicsMotor, "Gon:1-Ax:Y3}Mtr", {}),
+            "z": (EpicsMotor, "Gon:1-Ax:Z3}Mtr", {}),
+        }
+    )
+    fs = DDC(
+        {
+            "y": (EpicsMotor, "Gon:1-Ax:Visual}Mtr", {}),
+        }
+    )
+
+
+class BCU(Device):
+    slit_us = DDC(
+        {
+            "ib": (EpicsMotor, "Slt:BCUU-Ax:I}Mtr", {}),
+            "ob": (EpicsMotor, "Slt:BCUU-Ax:O}Mtr", {}),
+            "bb": (EpicsMotor, "Slt:BCUU-Ax:B}Mtr", {}),
+            "tb": (EpicsMotor, "Slt:BCUU-Ax:T}Mtr", {}),
+            "hg": (EpicsMotor, "Slt:BCUU-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:BCUU-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:BCUU-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:BCUU-Ax:VC}Mtr", {}),
+        }
+    )
+    slit_ds = DDC(
+        {
+            "ib": (EpicsMotor, "Slt:BCUD-Ax:I}Mtr", {}),
+            "ob": (EpicsMotor, "Slt:BCUD-Ax:O}Mtr", {}),
+            "bb": (EpicsMotor, "Slt:BCUD-Ax:B}Mtr", {}),
+            "tb": (EpicsMotor, "Slt:BCUD-Ax:T}Mtr", {}),
+            "hg": (EpicsMotor, "Slt:BCUD-Ax:HG}Mtr", {}),
+            "hc": (EpicsMotor, "Slt:BCUD-Ax:HC}Mtr", {}),
+            "vg": (EpicsMotor, "Slt:BCUD-Ax:VG}Mtr", {}),
+            "vc": (EpicsMotor, "Slt:BCUD-Ax:VC}Mtr", {}),
+        }
+    )
+    ilcam = DDC(
+        {
+            "x": (EpicsMotor, "Qstar:1-Ax:1}Mtr", {}),
+            "y": (EpicsMotor, "Qstar:1-Ax:2}Mtr", {}),
+            "z": (EpicsMotor, "Qstar:1-Ax:3}Mtr", {}),
+        }
+    )
--- tests/test_motors.py
+++ tests/test_motors.py
@@ -6,32 +6,24 @@ from collections.abc import Generator
 from subprocess import PIPE, Popen
 
 import pytest
+from ophyd import Device, EpicsSignal
 
 from cditools.motors import (
-    BPMDM3,
-    BPMDM4,
-    FSDM2,
-    FSDM3,
-    FSHPM,
-    FSVPM,
-    IMDM2,
-    FltrDM1,
-    Gon1,
-    MirHPM,
-    MirKBh,
-    MirKBv,
-    MirVPM,
-    MonoDMM,
-    MonoHDCM,
-    Qstar1,
-    SltBCUD,
-    SltBCUU,
-    SltDM3,
-    SltHPM,
-    SltVPM,
-    SltWB1,
-    WndExit,
+    BCU,
+    DCM,
+    DM1,
+    DM2,
+    DM3,
+    DM4,
+    DMM,
+    GON,
+    HPM,
+    KB,
+    VPM,
 )
+
+EpicsSignal.set_defaults(timeout=20.0, connection_timeout=20.0, write_timeout=20.0)
+Device.set_defaults(connection_timeout=20.0)
 
 
 @pytest.fixture(scope="session")
@@ -62,71 +54,35 @@ def black_hole_ioc() -> Generator[None, None, None]:
 
 
 def test_motors_can_connect(black_hole_ioc: None) -> None:
-    slt_wb1 = SltWB1(prefix="XF:09IDA-OP:1{Slt:WB1", name="slt_wb1")
-    slt_wb1.wait_for_connection(timeout=10.0)
+    dm1 = DM1(prefix="XF:09IDA-OP:1{", name="dm1")
+    dm1.wait_for_connection(timeout=10.0)
 
-    fltr_dm1 = FltrDM1(prefix="XF:09IDA-OP:1{Fltr:DM1", name="fltr_dm1")
-    fltr_dm1.wait_for_connection(timeout=10.0)
+    vpm = VPM(prefix="XF:09IDA-OP:1{", name="vpm")
+    vpm.wait_for_connection(timeout=10.0)
 
-    fsvpm = FSVPM(prefix="XF:09IDA-OP:1{FS:VPM", name="fsvpm")
-    fsvpm.wait_for_connection(timeout=10.0)
+    hpm = HPM(prefix="XF:09IDA-OP:1{", name="hpm")
+    hpm.wait_for_connection(timeout=10.0)
 
-    fshpm = FSHPM(prefix="XF:09IDA-OP:1{FS:HPM", name="fshpm")
-    fshpm.wait_for_connection(timeout=10.0)
+    dm2 = DM2(prefix="XF:09IDA-OP:1{", name="dm2")
+    dm2.wait_for_connection(timeout=10.0)
 
-    fsdm2 = FSDM2(prefix="XF:09IDA-OP:1{FS:DM2", name="fsdm2")
-    fsdm2.wait_for_connection(timeout=10.0)
+    dmm = DMM(prefix="XF:09IDA-OP:1{", name="dmm")
+    dmm.wait_for_connection(timeout=10.0)
 
-    mono_dmm = MonoDMM(prefix="XF:09IDA-OP:1{Mono:DMM", name="mono_dmm")
-    mono_dmm.wait_for_connection(timeout=10.0)
+    dcm = DCM(prefix="XF:09IDA-OP:1{", name="dcm")
+    dcm.wait_for_connection(timeout=10.0)
 
-    mono_hdcm = MonoHDCM(prefix="XF:09IDA-OP:1{Mono:HDCM", name="mono_hdcm")
-    mono_hdcm.wait_for_connection(timeout=10.0)
+    dm3 = DM3(prefix="XF:09IDB-OP:1{", name="dm3")
+    dm3.wait_for_connection(timeout=10.0)
 
-    slt_vpm = SltVPM(prefix="XF:09IDA-OP:1{Slt:VPM", name="slt_vpm")
-    slt_vpm.wait_for_connection(timeout=10.0)
+    kb = KB(prefix="XF:09IDC-OP:1{", name="kb")
+    kb.wait_for_connection(timeout=10.0)
 
-    slt_hpm = SltHPM(prefix="XF:09IDA-OP:1{Slt:HPM", name="slt_hpm")
-    slt_hpm.wait_for_connection(timeout=10.0)
+    dm4 = DM4(prefix="XF:09IDC-OP:1{", name="dm4")
+    dm4.wait_for_connection(timeout=10.0)
 
-    mir_vpm = MirVPM(prefix="XF:09IDA-OP:1{Mir:VPM", name="mir_vpm")
-    mir_vpm.wait_for_connection(timeout=10.0)
+    gon = GON(prefix="XF:09IDC-OP:1{", name="gon")
+    gon.wait_for_connection(timeout=10.0)
 
-    imdm2 = IMDM2(prefix="XF:09IDA-OP:1{IM:DM2", name="imdm2")
-    imdm2.wait_for_connection(timeout=10.0)
-
-    mir_hpm = MirHPM(prefix="XF:09IDA-OP:1{Mir:HPM", name="mir_hpm")
-    mir_hpm.wait_for_connection(timeout=10.0)
-
-    slt_dm3 = SltDM3(prefix="XF:09IDB-OP:1{Slt:DM3", name="slt_dm3")
-    slt_dm3.wait_for_connection(timeout=10.0)
-
-    bpm_dm3 = BPMDM3(prefix="XF:09IDB-OP:1{BPM:DM3", name="bpm_dm3")
-    bpm_dm3.wait_for_connection(timeout=10.0)
-
-    fsdm3 = FSDM3(prefix="XF:09IDB-OP:1{FS:DM3", name="fsdm3")
-    fsdm3.wait_for_connection(timeout=10.0)
-
-    mir_kbv = MirKBv(prefix="XF:09IDC-OP:1{Mir:KBv", name="mir_kbv")
-    mir_kbv.wait_for_connection(timeout=10.0)
-
-    mir_kbh = MirKBh(prefix="XF:09IDC-OP:1{Mir:KBh", name="mir_kbh")
-    mir_kbh.wait_for_connection(timeout=10.0)
-
-    bpm_dm4 = BPMDM4(prefix="XF:09IDC-OP:1{BPM:DM4", name="bpm_dm4")
-    bpm_dm4.wait_for_connection(timeout=10.0)
-
-    wnd_exit = WndExit(prefix="XF:09IDC-OP:1{Wnd:Exit", name="wnd_exit")
-    wnd_exit.wait_for_connection(timeout=10.0)
-
-    gon1 = Gon1(prefix="XF:09IDC-OP:1{Gon:1", name="gon1")
-    gon1.wait_for_connection(timeout=10.0)
-
-    slt_bcuu = SltBCUU(prefix="XF:09IDC-OP:1{Slt:BCUU", name="slt_bcuu")
-    slt_bcuu.wait_for_connection(timeout=10.0)
-
-    slt_bcud = SltBCUD(prefix="XF:09IDC-OP:1{Slt:BCUD", name="slt_bcud")
-    slt_bcud.wait_for_connection(timeout=10.0)
-
-    qstar1 = Qstar1(prefix="XF:09IDC-OP:1{Qstar:1", name="qstar1")
-    qstar1.wait_for_connection(timeout=10.0)
+    bcu = BCU(prefix="XF:09IDC-OP:1{", name="bcu")
+    bcu.wait_for_connection(timeout=10.0)
