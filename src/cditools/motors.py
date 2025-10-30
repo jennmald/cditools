c93f9344e06e5ad9ab764d37cb02fe7eb3289d01
jennmald <jmaldonad@bnl.gov>
2025-10-30 16:31:06 -0400

    format

--- src/cditools/motors.py
+++ src/cditools/motors.py
@@ -195,9 +195,13 @@ class DMM(Device):
 
 class DCMBase(Device):
     pitch = Cpt(EpicsMotor, "Mono:HDCM-Ax:Pitch}Mtr")
-    fine = {"fpitch": Cpt(EpicsMotor, "Mono:HDCM-Ax:FP}Mtr"), "roll":Cpt(EpicsMotor, "Mono:HDCM-Ax:Roll}Mtr")}
+    fine = {
+        "fpitch": Cpt(EpicsMotor, "Mono:HDCM-Ax:FP}Mtr"),
+        "roll": Cpt(EpicsMotor, "Mono:HDCM-Ax:Roll}Mtr"),
+    }
     h = Cpt(EpicsMotor, "Mono:HDCM-Ax:TX}Mtr")
     v = Cpt(EpicsMotor, "Mono:HDCM-Ax:TY}Mtr")
+
 
 class Energy(PseudoPositioner):
     bragg = Cpt(EpicsMotor, "Mono:HDCM-Ax:Bragg}Mtr")
